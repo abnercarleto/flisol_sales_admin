f2c826f7d33dddf7e1809673f6e1013437f0cf7c
Abner Carleto <abner.carleto@ymail.com>
2018-04-28 01:58:32 -0300

    remove sale_producs from screens

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -4,4 +4,22 @@ class Sale < ApplicationRecord
   has_many :products, through: :sale_products
 
   validates :sale_at, presence: true
+
+  rails_admin do
+    list do
+      configure :sale_products do
+        hide
+      end
+    end
+    edit do
+      configure :sale_products do
+        hide
+      end
+    end
+    show do
+      configure :sale_products do
+        hide
+      end
+    end
+  end
 end
